de5f546ca60b563c7c4e157bfaecf25a7fed63fd
LegoPanda04 <benjamin.j.levine@gmail.com>
2024-11-13 19:27:19 -0500

    Added new classes and functions to solve trusses.
    At some point may want to merge the Point() class and the TrussPoint() classes, but that is a problem for later.

--- statics.py
+++ statics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import RegularPolygon, FancyArrowPatch, Circle
 
 class Vector:
     def __init__(self, comps, magnitude, unit_vector, angle_cosines, coords = np.zeros(3)):
@@ -156,6 +157,12 @@ class Vector:
 
 class Point:
     def __init__(self, x, y, z):
+        """
+        This is not the same as the truss point and WILL NOT WORK WITH IT.
+        :param x: x
+        :param y: y
+        :param z: z
+        """
         self.x = x
         self.y = y
         self.z = z
@@ -174,3 +181,279 @@ def line(a,b):
     :return: vector from a to b
     """
     return b - a
+
+
+class TrussPoint:
+    def __init__(self, coords, constraints, forces):
+        """
+        Point class to store all the different points in the truss.
+        Format is TrussPoint(coords=[x,y], constraints=[x,y], forces=[x,y])
+        :param coords: coordinates of the point: [x,y]
+        :param constraints: constraints on the point as 1-0 or True-False: [x,y]
+        :param forces: forces at the point: [x,y]
+        """
+        self.coords = np.array(coords)
+        self.constraints = np.array(constraints)
+        self.forces = np.array(forces)
+
+        # Class variable to store the resultant force if needed.
+        self.resultants = np.array([0, 0])
+
+        # Class variable that keeps track of which rows in the solving matrix belong to it.
+        self.i = None
+
+    def plot_horizontal(self, ax, maxload, magnitude, color):
+        """
+        Internal horizontal arrow function.
+        You should not need to mess with this.
+        :param ax: ax from the plot().
+        :param maxload: scaling value from the plot().
+        :param magnitude: what variable should be used for plotting?
+        :param color: color of the arrow.
+        """
+        offset_x = np.sign(magnitude[0]) * maxload
+        ax.add_patch(FancyArrowPatch((self.coords[0] - offset_x, self.coords[1]), (self.coords[0], self.coords[1]),
+                                     arrowstyle='->', mutation_scale=20, color=color, zorder=10))
+        ax.text(self.coords[0] - offset_x, self.coords[1], str(round(abs(magnitude[0]), 2)), ha='center', va='center',
+                zorder=10)
+
+    def plot_vertical(self, ax, maxload, magnitude, color):
+        """
+        Internal vertical arrow function.
+        You should not need to mess with this.
+        :param ax: ax from the plot().
+        :param maxload: scaling value from the plot().
+        :param magnitude: what variable should be used for plotting?
+        :param color: color of the arrow.
+        """
+        offset_y = np.sign(magnitude[1]) * maxload
+        ax.add_patch(FancyArrowPatch((self.coords[0], self.coords[1] - offset_y), (self.coords[0], self.coords[1]),
+                                     arrowstyle='->', mutation_scale=20, color=color, zorder=10))
+        ax.text(self.coords[0], self.coords[1] - offset_y, str(round(abs(magnitude[1]), 2)), ha='center', va='center',
+                zorder=10)
+
+    def plot(self, ax, aspect, is_solved):
+        """
+        Plots the forces on a point
+        :param ax: ax from the plot().
+        :param aspect: scaling value from the plot().
+        :param is_solved: boolean of whether to do the solved or unsolved version.
+        """
+        # Calculate maxload based on aspect
+        maxload = aspect / 10
+
+        # Fixed support
+        if self.constraints[0] == 1 and self.constraints[1] == 1:
+            if is_solved:
+                self.plot_horizontal(ax, maxload, self.resultants, 'blue')
+                self.plot_vertical(ax, maxload, self.resultants, 'blue')
+            else:
+                side_length = aspect / 20
+                h = side_length * np.sqrt(3) / 2
+                x_center = self.coords[0]
+                y_center = self.coords[1] - 2 * h / 3
+                ax.add_patch(
+                    RegularPolygon((x_center, y_center), numVertices=3, radius=side_length / np.sqrt(3), orientation=0,
+                                   color='b'))
+
+        # Horizontal roller
+        elif self.constraints[0] == 1 and self.constraints[1] == 0:
+            if is_solved:
+                self.plot_horizontal(ax, maxload, self.resultants, 'blue')
+            else:
+                radius = aspect / 40
+                ax.add_patch(Circle((self.coords[0] + radius, self.coords[1]), radius=radius, color='b'))
+
+        # Vertical roller
+        elif self.constraints[0] == 0 and self.constraints[1] == 1:
+            if is_solved:
+                self.plot_vertical(ax, maxload, self.resultants, 'blue')
+            else:
+                radius = aspect / 40
+                ax.add_patch(Circle((self.coords[0], self.coords[1] - radius), radius=radius, color='b'))
+
+        # Horizontal force load
+        if abs(self.forces[0]) > 0:
+            self.plot_horizontal(ax, maxload, self.forces, 'red')
+
+        # Vertical force load
+        if abs(self.forces[1]) > 0:
+            self.plot_vertical(ax, maxload, self.forces, 'red')
+
+
+class TrussMember:
+    def __init__(self, a, b):
+        """
+        Member class to store relationships between points.
+        If your labels are coming out upside down, try flipping the order you input a and b.
+        :param a: point a.
+        :param b: point b.
+        """
+        self.a = a
+        self.b = b
+        self.length = np.linalg.norm(self.b.coords - self.a.coords)
+        self.components = (self.b.coords - self.a.coords) / self.length
+
+        # Class variable to store the tension in the member once calculated.
+        self.tension = None
+
+    def plot(self, ax, colors, is_solved):
+        """
+        Plots the truss member and internal forces if applicable.
+        :param ax: ax from the plot().
+        :param colors: color of the truss member.
+        :param is_solved: boolean of whether to do the solved or unsolved version.
+        """
+        # Makes the x and y lists
+        x_values = [self.a.coords[0], self.b.coords[0]]
+        y_values = [self.a.coords[1], self.b.coords[1]]
+
+        # Plots the line
+        ax.plot(x_values, y_values, color=colors, linewidth=2)
+
+        if is_solved:
+            # Computes the midpoint and rotation angle.
+            midpoint = (self.a.coords + self.b.coords) / 2
+            deltas = self.b.coords - self.a.coords
+            angle = np.rad2deg(np.arctan2(deltas[1], deltas[0]))
+
+            # Plots text if needed
+            ax.text(midpoint[0], midpoint[1], str(round(self.tension, 2)), rotation=angle, ha='center', va='center')
+
+
+def extract_points(points):
+    """
+    Internal function to get the coordinates of the points out of the point class.
+    :param points: list of point classes.
+    :return: list of coordinates of the points.
+    """
+    return np.array([[point.coords[0], point.coords[1]] for point in points])
+
+
+class Truss:
+    def __init__(self, members):
+        """
+        Class containing all the truss members and is where the magic happens.
+        Format is Truss([AB, BC, AC, ...]).
+        There is currently not duplication checking.
+        You do not need to pass in the points as those are contained inside the member classes.
+        :param members: members of the truss as a list.
+        """
+        # Adds members as a self variable
+        self.members = members
+
+        # Creates an empty list of points
+        self.points = []
+
+        # Going through the list of members, add one copy of the point to the list, and update the point class to have an index pointing to the x and y respective indexes.
+        # This is what enables the ability to pass in just the members and not the points.
+        for mem in self.members:
+            if not mem.a in self.points:
+                self.points.append(mem.a)
+                mem.a.i = 2 * self.points.index(mem.a)
+            if not mem.b in self.points:
+                self.points.append(mem.b)
+                mem.b.i = 2 * self.points.index(mem.b)
+
+    def solve(self, print_final=False):
+        """
+        Function to solve the truss.
+        I may add options to display intermediate results at a later date.
+        There is no output by default as each member and point has it's values updated instead.
+        If you need the output, set print_final=True.
+        :param print_final: prints the final output if needed, default is False.
+        """
+        # This is entirely because I do not want to redo all the instances of points and members to have self in front of them.
+        points = self.points
+        members = self.members
+
+        # Calculating the amount of points and members we are dealing with.
+        num_points = len(points)
+        num_members = len(members)
+
+        # Creates an array A twice as big as the points.
+        # Creates an array b to hold the equals forces.
+        A = np.zeros((num_points * 2, num_points * 2))
+        b = np.zeros((num_points * 2, 1))
+
+        # Adds the components of the internal forces to the array A.
+        # This only operates on the left half, with each column being a member and each row being an x and y force at a point.
+        # The second entry is mirrored because the forces are opposite.
+        for j, mem in enumerate(members):
+            A[mem.a.i:mem.a.i + 2, j] = mem.components
+            A[mem.b.i:mem.b.i + 2, j] = -mem.components
+
+        # Starts the counter at the right half of A.
+        counter = num_members
+        for point in points:
+            # If there is a constraint than add a 1 to A.
+            if point.constraints[0] > 0:
+                A[point.i, counter] = point.constraints[0]
+                counter += 1
+            if point.constraints[1] > 0:
+                A[point.i + 1, counter] = point.constraints[1]
+                counter += 1
+
+            # Populates the b array with the forces stored in the point class.
+            b[point.i] = -point.forces[0]
+            b[point.i + 1] = -point.forces[1]
+
+        # Solves the equation
+        solved = np.linalg.solve(A, b)
+
+        # Populates the members to have the tension
+        for i, mem in enumerate(members):
+            mem.tension = solved[i, 0]
+
+        # Resets the counter from earlier.
+        counter = num_members
+        for point in points:
+            # The same logic from before but now we are storing the value of said forced.
+            if point.constraints[0] > 0:
+                point.resultants[0] = solved[counter, 0]
+                counter += 1
+            if point.constraints[1] > 0:
+                point.resultants[1] = solved[counter, 0]
+                counter += 1
+
+        if print_final:
+            # If you need it to return the matrix, change this to return solved
+            print(solved)
+
+    def plot(self, is_solved=False):
+        """
+        Plots the truss system.
+        This function DOES NOT call self.solve().
+        Make sure to call self.solve() if is_solved is True.
+        :param is_solved: boolean of whether to do the solved or unsolved version.
+        """
+        # This is entirely because I do not want to redo all the instances of points and members to have self in front of them.
+        points = self.points
+        members = self.members
+
+        # Sets colors for the members
+        if is_solved:
+            colors = [0.75, 0.75, 0.75]
+        else:
+            colors = [0.1, 0.1, 0.1]
+
+        # Sets the aspect scaling.
+        aspect = max(np.max(extract_points(points)[:, 0]) - np.min(extract_points(points)[:, 0]),
+                     np.max(extract_points(points)[:, 1]) - np.min(extract_points(points)[:, 1]))
+
+        # To be completely honest, I am not sure why we need to do plt.subplots(), but the patches won't work otherwise.
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+        plt.grid()
+
+        # Plots the truss members.
+        for member in members:
+            member.plot(ax, colors, is_solved)
+
+        # Plots the forces at each point.
+        for point in points:
+            point.plot(ax, aspect, is_solved)
+
+        # Does some final formatting and plots.
+        plt.axis('equal')
+        plt.show()
